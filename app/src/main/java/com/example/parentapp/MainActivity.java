864b2ddacbd6fb8dfd86b7de05caae0882d0db98
11727 <jwa337@sfu.ca>
2021-10-29 14:41:59 -0700

    [Nick WU] Added Count Down Timer
    - created model class: TimeManager
    - created helper class: NotificationHelper
    - created activity: Timer

--- app/src/main/java/com/example/parentapp/MainActivity.java
+++ app/src/main/java/com/example/parentapp/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.parentapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class   MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = Timer.makeLaunchIntent(MainActivity.this);
+                startActivity(i);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/parentapp/helper/NotificationHelper.java
@@ -0,0 +1,60 @@
+package com.example.parentapp.helper;
+
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.Intent;
+
+import androidx.core.app.NotificationCompat;
+
+import com.example.parentapp.R;
+import com.example.parentapp.Timer;
+
+//the code below was adapted from the Youtube video linked:
+// https://www.youtube.com/watch?v=ub4_f6ksxL0
+public class NotificationHelper extends ContextWrapper {
+    public static final String channelID = "channelID";
+    public static final String channelName = "Channel";
+
+    private NotificationManager manager;
+
+    public NotificationHelper(Context base) {
+        super(base);
+        createChanel();
+    }
+
+    public void createChanel() {
+        NotificationChannel channel = new NotificationChannel(channelID, channelName,
+                NotificationManager.IMPORTANCE_DEFAULT);
+        channel.enableLights(true);
+        channel.enableVibration(true);
+        channel.setLightColor(R.color.design_default_color_primary);
+        channel.setLockscreenVisibility(Notification.VISIBILITY_PRIVATE);
+
+        getManager().createNotificationChannel(channel);
+    }
+
+    public NotificationManager getManager() {
+        if (manager == null) {
+            manager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+
+        return manager;
+    }
+
+    public NotificationCompat.Builder getChannelNotification(String title, String msg) {
+        Intent resultIntent = Timer.makeLaunchIntent(this);
+        PendingIntent resultPendingIntent = PendingIntent.getActivity(this, 1,
+                resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        return new NotificationCompat.Builder(getApplicationContext(), channelID)
+                .setContentTitle(title)
+                .setContentText(msg)
+                .setSmallIcon(R.drawable.ic_notification)
+                .setAutoCancel(true)
+                .setContentIntent(resultPendingIntent);
+    }
+}
