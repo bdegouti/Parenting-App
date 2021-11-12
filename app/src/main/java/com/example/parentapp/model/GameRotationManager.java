64a7a300ca155c518ddbee259c7dfa469f11d8cd
Nguyen Ai Vy (Ivy) Tran <natran@sfu.ca>
2021-11-11 18:53:21 -0800

    Implement function getQueue() for GameRotationManager.

--- app/src/main/java/com/example/parentapp/model/GameRotationManager.java
+++ app/src/main/java/com/example/parentapp/model/GameRotationManager.java
@@ -1,5 +1,9 @@
 package com.example.parentapp.model;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+
 /**
  * GameRotationManager class is a auxiliary class that stores the information of the last child who gets to pick heads/tails.
  * This class stores the name and index of that child, and it communicates with ChildrenManager class to retrieve the
@@ -33,6 +37,24 @@ public class GameRotationManager {
     public void setNameOfChildLastPicked(String name)
     {
         nameOfChildLastPicked = name;
+    }
+
+    public List<Child> getQueue(ChildrenManager childrenManager)
+    {
+        List<Child> queue = new ArrayList<>();
+        String nameCurrentChildWaiting = getNameNextChildToPickHeadTail(childrenManager);
+        int indexCurrentChildWaiting = childrenManager.getIndexOfChildName(nameCurrentChildWaiting);
+
+        int size = childrenManager.getNumberOfChildren();
+        int startIndex = (indexCurrentChildWaiting + 1) % size;
+
+        for(int i=0; i <= size-2; i++)
+        {
+            int currentIndex = (startIndex + i) % size;
+            Child child = childrenManager.getChildAtIndex(currentIndex);
+            queue.add(child);
+        }
+        return queue;
     }
 
 }
